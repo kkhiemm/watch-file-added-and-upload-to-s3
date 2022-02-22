775496390026b1b53b8fab164c3a47102d890e2f
Fleetlog CICD <info@joshwilson.com.au>
2022-02-22 16:26:47 +0700

    update function exactly

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const AWS = require("aws-sdk");
 const chokidar = require("chokidar");
 
-var s3;
+// TODO: env accessKeyId, secretAccessKey, Bucket
 
 const getS3 = () => {
-  AWS.config.update({ region: "ap-southeast-2" });
-  AWS.config.credentials = new AWS.SharedIniFileCredentials({
-    profile: "default",
+  const res = new AWS.S3({
+    accessKeyId: "accessKeyId",
+    secretAccessKey: "secretAccessKey",
   });
-  const res = new AWS.S3({ apiVersion: "2006-03-01" });
+
   return res;
 };
 
-const upload = (file) => {
-  var uploadParams = { Bucket: "mqmint", Key: "", Body: "" };
+const uploadToS3 = (s3, file) => {
+  var uploadParams = {
+    Bucket: "Bucket",
+    Key: "",
+    ACL: "public-read",
+    // ContentType: "image/jpeg",
+    Body: "",
+  };
 
   // Configure the file stream and obtain the upload parameters
   var fs = require("fs");
@@ -22,13 +28,11 @@ const upload = (file) => {
     console.log("File Error", err);
   });
   uploadParams.Body = fileStream;
+
+  // TODO: optimize
   // var path = require("path");
-  let arr = file.split(
-    "..\\watch-file-created-and-upload-to-s3_file-to-test\\"
-  );
-  console.log(arr[arr.length - 1]);
-  uploadParams.Key = arr[arr.length - 1].replace("\\", "/");
-  // console.log(uploadParams);
+  let arr = file.split("..\\camera-transfer-server\\");
+  uploadParams.Key = arr[arr.length - 1].replace("\\", "/").slice(3);
 
   // call S3 to retrieve upload file to specified bucket
   s3.upload(uploadParams, function (err, data) {
@@ -41,14 +45,16 @@ const upload = (file) => {
   });
 };
 
-s3 = getS3();
+const main = async () => {
+  const s3 = getS3();
 
-// One-liner for current directory
-chokidar
-  .watch("../watch-file-created-and-upload-to-s3_file-to-test")
-  .on("all", (event, path) => {
+  // One-liner for current directory
+  chokidar.watch("../camera-transfer-server").on("all", (event, path) => {
     console.log(event, path);
-    if (event == "add" && path.endsWith("txt")) {
-      upload(path);
+    if (event == "add" && (path.endsWith("jpeg") || path.endsWith("mp4"))) {
+      uploadToS3(s3, path);
     }
   });
+};
+
+main();
