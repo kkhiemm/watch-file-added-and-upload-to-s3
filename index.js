15a3c32c0dcc7d8bff5b1319f94770db2776a78c
Khiem Nguyen <khiemnd@newwave.com.vn>
2023-12-26 14:30:24 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 const chokidar = require("chokidar");
 
-// TODO: env accessKeyId, secretAccessKey, Bucket
+// TODO: env accessKeyId, secretAccessKey, Bucket, mysql db
 
 const getS3 = () => {
   const res = new AWS.S3({
@@ -31,7 +31,8 @@ const uploadToS3 = (s3, file, contentType, cb) => {
 
   // TODO: optimize
   // var path = require("path");
-  let arr = file.split("..\\camera-transfer-server\\");
+  // let arr = file.split("..\\camera-transfer-server\\");
+  let arr = file.split("..\\camera-footage-download-server-source-0.2.1\\downloads\\");
   uploadParams.Key = arr[arr.length - 1].replace("\\", "/").slice(3);
 
   // call S3 to retrieve upload file to specified bucket
@@ -77,10 +78,11 @@ const main = async () => {
 
   // One-liner for current directory
   chokidar
-    .watch("../camera-transfer-server", { ignoreInitial: true })
+    // .watch("../camera-transfer-server", { ignoreInitial: true })
+    .watch("../camera-footage-download-server-source-0.2.1/downloads", { ignoreInitial: true })
     .on("all", (event, path) => {
       console.log(event, path);
-      if (event == "add" && (path.endsWith("jpeg") || path.endsWith("mp4"))) {
+      if ((event == "add" || event == "change") && (path.endsWith("jpeg") || path.endsWith("mp4"))) {
         const contentType = path.endsWith("mp4") ? "video/mp4" : "image/jpeg";
         let arr = path.split("/"); // linux
         // let arr = path.split("\\"); // windows
